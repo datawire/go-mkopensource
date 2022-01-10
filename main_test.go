75b96a16f9f047f1fb4a040eeba613c1b4c929d5
Andres Osorio <aosorio@datawire.io>
2022-01-10 10:55:56 -0500

    Check program output to make sure license information is correct

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main_test
 
 import (
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
@@ -27,27 +28,62 @@ func TestGold(t *testing.T) {
 			defer func() {
 				require.NoError(t, os.Chdir(root))
 			}()
+
 			require.NoError(t, os.Chdir(filepath.Join("testdata", name)))
 
-			expErr, err := os.ReadFile("expected_err.txt")
-			if err != nil && !os.IsNotExist(err) {
-				require.NoError(t, err)
-			}
+			expectedError := getExpectedError(t)
+			expectedOutput := getExpectedOutput(t)
+
+			originalStdOut, r, w := interceptStdOut()
+			defer func() {
+				os.Stdout = originalStdOut
+			}()
 
 			actErr := main.Main(&main.CLIArgs{
 				OutputFormat:  "txt",
 				GoTarFilename: filepath.Join("..", "go1.17.3-testdata.src.tar.gz"),
 				Package:       "mod",
 			})
-			if expErr == nil {
-				assert.NoError(t, actErr)
+
+			_ = w.Close()
+
+			if expectedError == nil {
+				require.NoError(t, actErr)
+
+				programOutput, readErr := ioutil.ReadAll(r)
+				require.NoError(t, readErr)
+				assert.Equal(t, expectedOutput, programOutput)
 			} else {
 				if assert.Error(t, actErr) {
 					// Use this instead of assert.EqualError so that we diff
 					// output, which is helpful for long strings.
-					assert.Equal(t, strings.TrimSpace(string(expErr)), actErr.Error())
+					assert.Equal(t, strings.TrimSpace(string(expectedError)), actErr.Error())
 				}
 			}
 		})
 	}
+}
+
+func interceptStdOut() (originalStdOut *os.File, r *os.File, w *os.File) {
+	originalStdOut = os.Stdout
+
+	r, w, _ = os.Pipe()
+	os.Stdout = w
+	return originalStdOut, r, w
+}
+
+func getExpectedError(t *testing.T) []byte {
+	expErr, err := os.ReadFile("expected_err.txt")
+	if err != nil && !os.IsNotExist(err) {
+		require.NoError(t, err)
+	}
+	return expErr
+}
+
+func getExpectedOutput(t *testing.T) []byte {
+	expectedOutput, err := os.ReadFile("expected_output.txt")
+	if err != nil && !os.IsNotExist(err) {
+		require.NoError(t, err)
+	}
+	return expectedOutput
 }
