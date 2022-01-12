a388736a59cfa342c67a403e0d48d6c6a7fb67cd
Andres Osorio <aosorio@datawire.io>
2022-01-11 21:54:54 -0500

    Parameterize tests for main package

--- main_test.go
+++ main_test.go
@@ -24,12 +24,6 @@ func TestGolden(t *testing.T) {
 	}{
 		{
 			testName:       "",
-			testData:       "testdata/00-intern-old",
-			packagesFlag:   "mod",
-			outputTypeFlag: "full",
-		},
-		{
-			testName:       "",
 			testData:       "testdata/01-intern-new",
 			packagesFlag:   "mod",
 			outputTypeFlag: "full",
@@ -37,12 +31,6 @@ func TestGolden(t *testing.T) {
 		{
 			testName:       "",
 			testData:       "testdata/02-replace",
-			packagesFlag:   "mod",
-			outputTypeFlag: "full",
-		},
-		{
-			testName:       "",
-			testData:       "testdata/03-multierror",
 			packagesFlag:   "mod",
 			outputTypeFlag: "full",
 		},
@@ -66,8 +54,7 @@ func TestGolden(t *testing.T) {
 		},
 	}
 
-	workingDir, err := os.Getwd()
-	require.NoError(t, err)
+	workingDir := getWorkingDir(t)
 
 	for _, testCase := range testCases {
 		t.Run(testCase.testName, func(t *testing.T) {
@@ -76,8 +63,6 @@ func TestGolden(t *testing.T) {
 			}()
 
 			require.NoError(t, os.Chdir(testCase.testData))
-
-			expectedError := getFileContents(t, "expected_err.txt")
 
 			originalStdOut, r, w := interceptStdOut()
 			defer func() {
@@ -93,23 +78,67 @@ func TestGolden(t *testing.T) {
 
 			_ = w.Close()
 
-			if expectedError == nil {
-				require.NoError(t, actErr)
+			require.NoError(t, actErr)
 
-				programOutput, readErr := io.ReadAll(r)
-				require.NoError(t, readErr)
+			programOutput, readErr := io.ReadAll(r)
+			require.NoError(t, readErr)
 
-				expectedOutput := getFileContents(t, "expected_full_output.txt")
-				assert.Equal(t, string(expectedOutput), string(programOutput))
-			} else {
-				if assert.Error(t, actErr) {
-					// Use this instead of assert.EqualError so that we diff
-					// output, which is helpful for long strings.
-					assert.Equal(t, strings.TrimSpace(string(expectedError)), actErr.Error())
-				}
+			expectedOutput := getFileContents(t, "expected_full_output.txt")
+			assert.Equal(t, string(expectedOutput), string(programOutput))
+		})
+	}
+}
+
+func TestErrorScenarios(t *testing.T) {
+	testCases := []struct {
+		testData       string
+		packagesFlag   string
+		outputTypeFlag string
+	}{
+		{
+			testData:       "testdata/00-intern-old",
+			packagesFlag:   "mod",
+			outputTypeFlag: "full",
+		},
+		{
+			testData:       "testdata/03-multierror",
+			packagesFlag:   "mod",
+			outputTypeFlag: "full",
+		},
+	}
+
+	workingDir := getWorkingDir(t)
+
+	for _, testCase := range testCases {
+		t.Run(testCase.testData, func(t *testing.T) {
+			defer func() {
+				require.NoError(t, os.Chdir(workingDir))
+			}()
+
+			require.NoError(t, os.Chdir(testCase.testData))
+
+			expectedError := getFileContents(t, "expected_err.txt")
+
+			actErr := main.Main(&main.CLIArgs{
+				OutputFormat:  "txt",
+				GoTarFilename: filepath.Join("..", "go1.17.3-testdata.src.tar.gz"),
+				Package:       testCase.packagesFlag,
+				OutputType:    testCase.outputTypeFlag,
+			})
+
+			if assert.Error(t, actErr) {
+				// Use this instead of assert.EqualError so that we diff
+				// output, which is helpful for long strings.
+				assert.Equal(t, strings.TrimSpace(string(expectedError)), actErr.Error())
 			}
 		})
 	}
+}
+
+func getWorkingDir(t *testing.T) string {
+	workingDir, err := os.Getwd()
+	require.NoError(t, err)
+	return workingDir
 }
 
 func TestLicenseTxtOutput(t *testing.T) {
