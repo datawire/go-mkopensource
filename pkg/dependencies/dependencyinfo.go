0baab9f29adf6bdf8f4b4a3363556e3435a0ce67
Andres Osorio <aosorio@datawire.io>
2022-01-24 14:17:16 -0500

    Implement logic to check for allowed and forbidden licenses

--- pkg/dependencies/dependencyinfo.go
+++ pkg/dependencies/dependencyinfo.go
@@ -7,20 +7,33 @@ import (
 )
 
 //nolint:gochecknoglobals // Can't be a constant
-var knownLicenses = map[string]detectlicense.License{
-	detectlicense.Proprietary.Name:   detectlicense.Proprietary,
-	detectlicense.PublicDomain.Name:  detectlicense.PublicDomain,
+var licensesByName = map[string]detectlicense.License{
 	detectlicense.Apache2.Name:       detectlicense.Apache2,
+	detectlicense.AGPL1Only.Name:     detectlicense.AGPL1Only,
+	detectlicense.AGPL1OrLater.Name:  detectlicense.AGPL1OrLater,
+	detectlicense.AGPL3Only.Name:     detectlicense.AGPL3Only,
+	detectlicense.AGPL3OrLater.Name:  detectlicense.AGPL3OrLater,
 	detectlicense.BSD1.Name:          detectlicense.BSD1,
 	detectlicense.BSD2.Name:          detectlicense.BSD2,
 	detectlicense.BSD3.Name:          detectlicense.BSD3,
 	detectlicense.CcBySa40.Name:      detectlicense.CcBySa40,
-	detectlicense.GPL3.Name:          detectlicense.GPL3,
+	detectlicense.GPL1Only.Name:      detectlicense.GPL1Only,
+	detectlicense.GPL1OrLater.Name:   detectlicense.GPL1OrLater,
+	detectlicense.GPL2Only.Name:      detectlicense.GPL2Only,
+	detectlicense.GPL2OrLater.Name:   detectlicense.GPL2OrLater,
+	detectlicense.GPL3Only.Name:      detectlicense.GPL3Only,
+	detectlicense.GPL3OrLater.Name:   detectlicense.GPL3OrLater,
 	detectlicense.ISC.Name:           detectlicense.ISC,
+	detectlicense.LGPL2Only.Name:     detectlicense.LGPL2Only,
+	detectlicense.LGPL2OrLater.Name:  detectlicense.LGPL2OrLater,
+	detectlicense.LGPL21Only.Name:    detectlicense.LGPL21Only,
 	detectlicense.LGPL21OrLater.Name: detectlicense.LGPL21OrLater,
+	detectlicense.LGPL3Only.Name:     detectlicense.LGPL3Only,
+	detectlicense.LGPL3OrLater.Name:  detectlicense.LGPL3OrLater,
 	detectlicense.MIT.Name:           detectlicense.MIT,
 	detectlicense.MPL2.Name:          detectlicense.MPL2,
 	detectlicense.PSF.Name:           detectlicense.PSF,
+	detectlicense.PublicDomain.Name:  detectlicense.PublicDomain,
 	detectlicense.Unicode2015.Name:   detectlicense.Unicode2015}
 
 type DependencyInfo struct {
@@ -54,9 +67,9 @@ func (d *DependencyInfo) UpdateLicenseList() error {
 
 	for _, dependency := range d.Dependencies {
 		for _, licenseName := range dependency.Licenses {
-			license, ok := knownLicenses[licenseName]
-			if !ok {
-				return fmt.Errorf("license details for '%s' are not known", licenseName)
+			license, err := getLicenseFromName(licenseName)
+			if err != nil {
+				return err
 			}
 			usedLicenses[license.Name] = license
 		}
@@ -66,5 +79,40 @@ func (d *DependencyInfo) UpdateLicenseList() error {
 		d.Licenses[k] = v.URL
 	}
 
+	return nil
+}
+
+func getLicenseFromName(licenseName string) (detectlicense.License, error) {
+	license, ok := licensesByName[licenseName]
+	if !ok {
+		return detectlicense.License{}, fmt.Errorf("license details for '%s' are not known", licenseName)
+	}
+	return license, nil
+}
+
+// CheckLicenses checks that the licenses used by the dependencies are known and allowed to be used
+//in an application based on the buiness logic described here: https://www.notion.so/datawire/License-Management-5194ca50c9684ff4b301143806c92157.
+//This function must be called after parsing of the licenses has been done.
+func (d *DependencyInfo) CheckLicenses(allowedLicenses detectlicense.AllowedLicenseUse) error {
+	if allowedLicenses == detectlicense.Forbidden {
+		return fmt.Errorf("forbidden licenses should not be used")
+	}
+
+	for _, dependency := range d.Dependencies {
+		for _, licenseName := range dependency.Licenses {
+			license, err := getLicenseFromName(licenseName)
+			if err != nil {
+				return err
+			}
+
+			if license.AllowedUse == detectlicense.Forbidden {
+				return fmt.Errorf("license '%s' is forbidden", license.Name)
+			}
+
+			if license.AllowedUse < allowedLicenses {
+				return fmt.Errorf("license '%s' should not be used since it doesn't meet the useage requirements", license.Name)
+			}
+		}
+	}
 	return nil
 }
--- pkg/dependencies/dependencyinfo_test.go
+++ pkg/dependencies/dependencyinfo_test.go
@@ -35,7 +35,7 @@ var (
 			{
 				Name:     "library1",
 				Version:  "1.0.2",
-				Licenses: []string{detectlicense.GPL3.Name, detectlicense.BSD2.Name},
+				Licenses: []string{detectlicense.GPL3Only.Name, detectlicense.BSD2.Name},
 			},
 		},
 		Licenses: map[string]string{},
@@ -46,7 +46,7 @@ var (
 			{
 				Name:     "library1",
 				Version:  "1.0.2",
-				Licenses: []string{detectlicense.GPL3.Name, detectlicense.BSD2.Name},
+				Licenses: []string{detectlicense.GPL3Only.Name, detectlicense.BSD2.Name},
 			},
 			{
 				Name:     "library2",
@@ -56,7 +56,7 @@ var (
 			{
 				Name:     "library2",
 				Version:  "3.1.2",
-				Licenses: []string{detectlicense.Apache2.Name, detectlicense.GPL3.Name},
+				Licenses: []string{detectlicense.Apache2.Name, detectlicense.GPL3Only.Name},
 			},
 		},
 		Licenses: map[string]string{},
@@ -68,6 +68,71 @@ var (
 				Name:     "library1",
 				Version:  "1.0.2",
 				Licenses: []string{detectlicense.PublicDomain.Name},
+			},
+		},
+		Licenses: map[string]string{},
+	}
+
+	forbiddenLicensesOnly = dependencies.DependencyInfo{
+		Dependencies: []dependencies.Dependency{
+			{
+				Name:     "library1",
+				Version:  "1.0.2",
+				Licenses: []string{detectlicense.AGPL1Only.Name},
+			},
+		},
+		Licenses: map[string]string{},
+	}
+
+	unrestrictedLicensesOnly = dependencies.DependencyInfo{
+		Dependencies: []dependencies.Dependency{
+			{
+				Name:     "library1",
+				Version:  "1.0.2",
+				Licenses: []string{detectlicense.MIT.Name},
+			},
+		},
+		Licenses: map[string]string{},
+	}
+
+	licensesForAmbassadorServersOnly = dependencies.DependencyInfo{
+		Dependencies: []dependencies.Dependency{
+			{
+				Name:     "library1",
+				Version:  "1.0.2",
+				Licenses: []string{detectlicense.GPL3Only.Name},
+			},
+		},
+		Licenses: map[string]string{},
+	}
+
+	mixOfLicensesIncludingForbidden = dependencies.DependencyInfo{
+		Dependencies: []dependencies.Dependency{
+			{
+				Name:     "library1",
+				Version:  "1.0.2",
+				Licenses: []string{detectlicense.GPL3Only.Name},
+			},
+			{
+				Name:     "library2",
+				Version:  "3.1.3",
+				Licenses: []string{detectlicense.MIT.Name},
+			},
+			{
+				Name:     "library3",
+				Version:  "1.3.5",
+				Licenses: []string{detectlicense.AGPL1Only.Name},
+			},
+		},
+		Licenses: map[string]string{},
+	}
+
+	mixOfLicensesWithoutForbidden = dependencies.DependencyInfo{
+		Dependencies: []dependencies.Dependency{
+			{
+				Name:     "library1",
+				Version:  "1.0.2",
+				Licenses: []string{detectlicense.GPL3Only.Name, detectlicense.MIT.Name},
 			},
 		},
 		Licenses: map[string]string{},
@@ -96,18 +161,18 @@ func TestLicenseListIsCorrect(t *testing.T) {
 			"A dependency with multiple licenses",
 			dependencyWithMultipleLicenses,
 			map[string]string{
-				detectlicense.GPL3.Name: detectlicense.GPL3.URL,
-				detectlicense.BSD2.Name: detectlicense.BSD2.URL,
+				detectlicense.GPL3Only.Name: detectlicense.GPL3Only.URL,
+				detectlicense.BSD2.Name:     detectlicense.BSD2.URL,
 			},
 		},
 		{
 			"Dependencies with overlapping licenses",
 			dependenciesWithOverlappingLicenses,
 			map[string]string{
-				detectlicense.GPL3.Name:    detectlicense.GPL3.URL,
-				detectlicense.BSD2.Name:    detectlicense.BSD2.URL,
-				detectlicense.Apache2.Name: detectlicense.Apache2.URL,
-				detectlicense.GPL3.Name:    detectlicense.GPL3.URL,
+				detectlicense.GPL3Only.Name: detectlicense.GPL3Only.URL,
+				detectlicense.BSD2.Name:     detectlicense.BSD2.URL,
+				detectlicense.Apache2.Name:  detectlicense.Apache2.URL,
+				detectlicense.GPL3Only.Name: detectlicense.GPL3Only.URL,
 			},
 		},
 		{
@@ -121,11 +186,97 @@ func TestLicenseListIsCorrect(t *testing.T) {
 
 	for _, testCase := range testCases {
 		t.Run(testCase.Name, func(t *testing.T) {
-			// No licenses does not generate an error
 			err := testCase.dependencies.UpdateLicenseList()
 			require.NoError(t, err)
 
 			require.Equal(t, testCase.expectedLicenses, testCase.dependencies.Licenses)
+		})
+	}
+}
+
+func TestCheckLicensesValidatesAllowedLicenseCorrectly(t *testing.T) {
+	testCases := []struct {
+		Name            string
+		dependencies    dependencies.DependencyInfo
+		allowedLicenses detectlicense.AllowedLicenseUse
+	}{
+		{
+			"Empty dependency list is always allowed",
+			emptyDependencies,
+			detectlicense.Unrestricted,
+		},
+		{
+			"Unrestricted licenses are OK on Ambassador Labs servers",
+			unrestrictedLicensesOnly,
+			detectlicense.OnAmbassadorServers,
+		},
+		{
+			"Unrestricted licenses are OK everywhere",
+			unrestrictedLicensesOnly,
+			detectlicense.Unrestricted,
+		},
+		{
+			"Restricted licenses are OK on Ambassador Labs servers",
+			licensesForAmbassadorServersOnly,
+			detectlicense.OnAmbassadorServers,
+		},
+		{
+			"Mix of licenses without forbidden is allowed on Ambassador Labs servers",
+			mixOfLicensesWithoutForbidden,
+			detectlicense.OnAmbassadorServers,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			err := testCase.dependencies.CheckLicenses(testCase.allowedLicenses)
+			require.NoError(t, err)
+		})
+	}
+}
+
+func TestCheckLicensesValidatesForbiddenLicensesCorrectly(t *testing.T) {
+	testCases := []struct {
+		Name            string
+		dependencies    dependencies.DependencyInfo
+		allowedLicenses detectlicense.AllowedLicenseUse
+	}{
+		{
+			"It's not possible to allow the use of forbidden licenses by mistake",
+			unrestrictedLicensesOnly,
+			detectlicense.Forbidden,
+		},
+		{
+			"Forbidden licenses are not allowed on Ambassador Labs servers",
+			forbiddenLicensesOnly,
+			detectlicense.OnAmbassadorServers,
+		},
+		{
+			"Forbidden licenses are not allowed on customer servers",
+			forbiddenLicensesOnly,
+			detectlicense.Unrestricted,
+		},
+		{
+			"Restricted licenses are not OK on customer servers",
+			licensesForAmbassadorServersOnly,
+			detectlicense.Unrestricted,
+		},
+		{
+			"Mix of licenses including forbidden is rejected in Ambassador Labs servers",
+			mixOfLicensesIncludingForbidden,
+			detectlicense.Unrestricted,
+		},
+		{
+			"Mix of licenses without forbidden is rejected on customer servers",
+			mixOfLicensesWithoutForbidden,
+			detectlicense.Unrestricted,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			err := testCase.dependencies.CheckLicenses(testCase.allowedLicenses)
+			require.Error(t, err)
 		})
 	}
 }
