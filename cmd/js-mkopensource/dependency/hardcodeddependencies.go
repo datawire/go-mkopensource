a0d3ccfb50a2217ac4c9ef704760ceeef9f26b1b
Andres Osorio <aosorio@datawire.io>
2022-02-04 14:21:42 -0500

    Add unknown license types found while scanning for NPM dependencies

--- cmd/js-mkopensource/dependency/hardcodeddependencies.go
+++ cmd/js-mkopensource/dependency/hardcodeddependencies.go
@@ -1,10 +1,17 @@
 package dependency
 
-import "github.com/datawire/go-mkopensource/pkg/detectlicense"
+import . "github.com/datawire/go-mkopensource/pkg/detectlicense"
 
 //nolint:gochecknoglobals // Would be 'const'.
 var hardcodedDependencies = map[string][]string{
-	"cyclist@0.2.2": {detectlicense.MIT.Name},
-	"indexof@0.0.1": {detectlicense.MIT.Name},
-	"pako@1.0.10":   {detectlicense.MIT.Name},
+	"cyclist@0.2.2":                {MIT.Name},
+	"doctrine@1.5.0":               {BSD2.Name, Apache2.Name},
+	"emitter-component@1.1.1":      {MIT.Name},
+	"flexboxgrid@6.3.1":            {Apache2.Name},
+	"indexof@0.0.1":                {MIT.Name},
+	"intro.js@4.1.0":               {AGPL3OrLater.Name},
+	"node-forge@0.10.0":            {BSD3.Name},
+	"pako@1.0.10":                  {MIT.Name},
+	"regenerator-transform@0.10.1": {BSD2.Name},
+	"regjsparser@0.1.5":            {BSD2.Name},
 }
