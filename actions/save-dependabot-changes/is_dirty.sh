fae66c611d613784eb3403bf088051569f87a580
Andres Osorio <aosorio@datawire.io>
2023-02-07 16:30:40 -0500

    Improve action logging

--- actions/save-dependabot-changes/is_dirty.sh
+++ actions/save-dependabot-changes/is_dirty.sh
@@ -2,14 +2,16 @@
 set -e
 
 if ! git diff --name-only --exit-code DEPENDENCIES.md; then
+    echo '::notice:: DEPENDENCIES.md and/or DEPENDENCY_LICENSES.md changed and they will be committed.'
     echo "DIRTY=true" >> $GITHUB_OUTPUT
     exit 0
 fi
 
 if ! git diff --name-only --exit-code DEPENDENCY_LICENSES.md; then
+    echo '::notice:: DEPENDENCIES.md and/or DEPENDENCY_LICENSES.md changed and they will be committed.'
     echo "DIRTY=true" >> $GITHUB_OUTPUT
     exit 0
 fi
 
-echo "There are no changes to save"
+echo "::debug:: There are no changes to save"
 echo "DIRTY=false" >> $GITHUB_OUTPUT
