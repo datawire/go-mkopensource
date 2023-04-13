193de59c4ca0eca9255dfe33a5a9fb42bdb952ae
Andres Osorio <aosorio@datawire.io>
2023-04-13 16:30:16 -0400

    Fix bug that broke passing a list of unparsable dependencies

--- build-aux/docker/scan-go.sh
+++ build-aux/docker/scan-go.sh
@@ -12,10 +12,10 @@ cd /app
 GO_VERSION=$(go version | sed -E 's/.*go([1-9\.]*).*/\1/')
 
 /scripts/go-mkopensource --output-format=txt --package=mod --output-type=markdown --gotar="$(ls /data/go*.src.tar.gz)" \
-  --unparsable-packages="$UNPARSABLE_PACKAGE_VALUE" >"${GO_DEPENDENCIES}"
+  --unparsable-packages="${UNPARSABLE_PACKAGE}" >"${GO_DEPENDENCIES}"
 
 DEPENDENCY_INFO="${BUILD_TMP}/go_dependencies.json"
 /scripts/go-mkopensource --output-format=txt --package=mod --output-type=json --application-type=${APPLICATION_TYPE} \
-  --unparsable-packages="$UNPARSABLE_PACKAGE_VALUE" --gotar="$(ls /data/go*.src.tar.gz)" >"${DEPENDENCY_INFO}"
+  --unparsable-packages="${UNPARSABLE_PACKAGE}" --gotar="$(ls /data/go*.src.tar.gz)" >"${DEPENDENCY_INFO}"
 
 jq -r '.licenseInfo | to_entries | .[] | "* [" + .key + "](" + .value + ")"' "${DEPENDENCY_INFO}" >"${GO_LICENSES}"
